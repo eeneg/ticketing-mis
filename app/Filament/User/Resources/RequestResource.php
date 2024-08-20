50c237f26a79a337167a07f8f811194a77079add
Joowd <joowdx@pm.me>
2024-08-20 14:04:36 +0800

    Fix select relationship query

--- app/Filament/User/Resources/RequestResource.php
+++ app/Filament/User/Resources/RequestResource.php
@@ -3,8 +3,6 @@
 namespace App\Filament\User\Resources;
 
 use App\Filament\User\Resources\RequestResource\Pages;
-use App\Models\Category;
-use App\Models\Office;
 use App\Models\Request;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -28,55 +26,38 @@ class RequestResource extends Resource
         return $form
             ->columns(2)
             ->schema([
-
                 Forms\Components\Select::make('office_id')
                     ->relationship('office', 'name')
                     ->columnSpan(2)
-                    ->options(Office::all()->pluck(value: 'name', key: 'id')->toArray())
+                    ->searchable()
+                    ->preload()
                     ->reactive()
                     ->afterStateUpdated(fn (callable $set) => $set('category_id', null)),
-
                 Forms\Components\Select::make('category_id')
                     ->required()
-                    ->relationship('category', 'name')
-                    ->options(function (callable $get) {
-                        $office = Office::find($get('office_id'));
-                        if (! $office) {
-                            return;
-                        } else {
-                            return $office->categories->pluck('name', 'id');
-                        }
-                    })
+                    ->relationship('category', 'name', fn (Builder $query, callable $get) => $query->where('office_id', $get('office_id')))
                     ->reactive()
+                    ->preload()
+                    ->searchable()
                     ->afterStateUpdated(fn (callable $set) => $set('subcategory_id', null)),
-
                 Forms\Components\Select::make('subcategory_id')
                     ->label('Subcategory')
-                    ->relationship('subcategory', 'name')
-                    ->options(function (callable $get) {
-                        $category = Category::find($get('category_id'));
-                        if (! $category) {
-                            return;
-                        } else {
-                            return $category->subcategories->pluck('name', 'id');
-                        }
-                    }),
+                    ->relationship('subcategory', 'name', fn (Builder $query, callable $get) => $query->where('category_id', $get('category_id')))
+                    ->preload()
+                    ->searchable(),
                 Forms\Components\RichEditor::make('remarks')
                     ->columnSpan(2)
                     ->label('Remarks')
                     ->placeholder('Describe the issue'),
-
                 Forms\Components\DateTimePicker::make('availability_from')
                     ->placeholder('24:00')
                     ->displayFormat('Y-m-d')
                     ->seconds(false),
-
                 Forms\Components\DateTimePicker::make('availability_to')
                     ->after('availability_from')
                     ->placeholder('24:00')
                     ->displayFormat('Y-m-d')
                     ->seconds(false),
-
                 Forms\Components\Hidden::make('requestor_id')
                     ->default(Auth::id()),
             ]);
@@ -86,17 +67,12 @@ class RequestResource extends Resource
     {
 
         return $table
-            ->modifyQueryUsing(function (Builder $query) {
-                // Query tickets only from the logged in non-admin user
-                $userId = Auth::id();
-                $query->where('requestor_id', $userId);
-            })
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('requestor_id', Auth::id()))
             ->columns([
                 Tables\Columns\TextColumn::make('office.name')->label('Office'),
                 Tables\Columns\TextColumn::make('category.name')->label('Category'),
                 Tables\Columns\TextColumn::make('subcategory.name')->label('Subcategory'),
                 Tables\Columns\TextColumn::make('requestor.name')->label('Requestor'),
-
             ])
             ->filters([
                 //
