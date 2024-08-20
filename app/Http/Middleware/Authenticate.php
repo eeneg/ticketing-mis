84b5dadf2b93ae7ed01b5634dab27bd684fe7f44
Joowd <joowdx@pm.me>
2024-08-20 11:59:52 +0800

    Fix login route not defined exception

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,42 @@
 
 namespace App\Http\Middleware;
 
+use Filament\Facades\Filament;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * @param  array<string>  $guards
      */
-    protected function redirectTo(Request $request): ?string
+    protected function authenticate($request, array $guards): void
     {
-        return $request->expectsJson() ? null : route('login');
+        $guard = Filament::auth();
+
+        if (! $guard->check()) {
+            $this->unauthenticated($request, $guards);
+
+            return;
+        }
+
+        $this->auth->shouldUse(Filament::getAuthGuard());
+
+        /** @var Model $user */
+        $user = $guard->user();
+
+        $panel = Filament::getCurrentPanel();
+
+        abort_if(
+            $user instanceof FilamentUser ?
+                (! $user->canAccessPanel($panel)) :
+                (config('app.env') !== 'local'),
+            403,
+        );
+    }
+
+    protected function redirectTo($request): ?string
+    {
+        return $request->expectsJson() ? null : route('filament.user.auth.login');
     }
 }
