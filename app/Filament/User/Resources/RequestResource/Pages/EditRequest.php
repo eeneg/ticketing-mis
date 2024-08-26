8a167085ffb9f37324be8f4c8882b95137a7b917
Joowd <joowdx@gmail.com>
2024-08-27 05:44:46 +0800

    Remove unused code and refactor Filament actions namespace

--- app/Filament/User/Resources/RequestResource/Pages/EditRequest.php
+++ app/Filament/User/Resources/RequestResource/Pages/EditRequest.php
@@ -2,12 +2,10 @@
 
 namespace App\Filament\User\Resources\RequestResource\Pages;
 
-use App\Enums\RequestStatus;
 use App\Filament\Actions\PublishRequestAction;
 use App\Filament\Actions\RetractRequestAction;
 use App\Filament\User\Resources\RequestResource;
 use Filament\Actions;
-use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,34 +20,6 @@ class EditRequest extends EditRecord
             RetractRequestAction::make(),
             Actions\DeleteAction::make(),
         ];
-    }
-
-    protected function getFormActions(): array
-    {
-        return [
-            $this->getSaveFormAction(),
-
-            $this->getCancelFormAction(),
-
-        ];
-    }
-
-    public function retract()
-    {
-        $this->record->update([
-            'published_at' => null,
-        ]);
-        $this->record->actions()->create([
-            'request_id' => $this->record->id,
-            'user_id' => Auth::id(),
-            'status' => RequestStatus::RETRACTED,
-            'remarks' => $this->record->remarks,
-            'time' => now(),
-        ]);
-        Notification::make()
-            ->title('Request Retracted Successfully')
-            ->success()
-            ->send();
     }
 
     protected function mutateFormDataBeforeSave(array $data): array
